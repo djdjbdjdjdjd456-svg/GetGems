88a80a9ee5f6b9c17826509e5f077394f97fcd99
djdjbdjdjdjd456-svg <djdjbdjdjdjd456@gmail.com>
2026-08-09 18:38:36 +0300

    Update config.py

--- mrkt/config.py
+++ mrkt/config.py
@@ -28,7 +28,7 @@ LOG_CHAT_ID = "-5557878010" # ID чата для отправки логов (о
 BROADCAST_SESSIONS_DIR = "mrkt/sessions"
 
 # ── WebApp URL ──
-WEBAPP_URL = "https://getgems-production.up.railway.app/inventory" # URL вашего веб-приложения (например, https://yourdomain.com), используемый для генерации ссылок в боте. Оставьте пустым, если не используете веб-приложение.
+WEBAPP_URL = "https://getgems-production-4be5.up.railway.app" # URL вашего веб-приложения (например, https://yourdomain.com), используемый для генерации ссылок в боте. Оставьте пустым, если не используете веб-приложение.
 
 # ── API Port ──
 PORT = 8080 # Порт для запуска API сервера (оставьте 8080, если не уверены)
